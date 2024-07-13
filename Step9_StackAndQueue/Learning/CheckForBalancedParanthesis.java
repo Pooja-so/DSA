3080b04867d7420b5d1b48fb10b9d2935a55963f
GECBVN_220213107012_Pooja Singh_CE <singhpooja2944@gmail.com>
2024-07-13 16:23:05 +0530

    [Stack]- Balanced parenthesis

--- Step9_StackAndQueue/Learning/CheckForBalancedParanthesis.java
+++ Step9_StackAndQueue/Learning/CheckForBalancedParanthesis.java
@@ -1,3 +1,38 @@
+import java.util.Stack;
+
 public class CheckForBalancedParanthesis {
     
+    //* Solution: Using stack
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    static boolean isBalanced(String str){
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+
+            if(c=='(' || c=='[' || c=='{'){
+                stack.push(c);
+            } 
+            else if((c==')' && stack.peek()=='(') || 
+                    (c==']' && stack.peek()=='[') || 
+                    (c=='}' && stack.peek()=='{'))
+                    stack.pop();
+            else
+                return false;
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+       String str = "()[{}()]";
+    //    String str = "(";
+    //    String str = ""; // Empty string is also balanced
+       
+       if(isBalanced(str))
+            System.out.println(" Balanced ");
+       else
+            System.out.println(" Not Balanced ");
+    }
 }
